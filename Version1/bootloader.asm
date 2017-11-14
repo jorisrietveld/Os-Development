85cc763a73e2f2f456d50a68d9ae5942e61cf558
joris <jorisrietveld@gmail.com>
2017-11-14 01:33:29 +0100

    Finished first bootloader.

--- Version1/bootloader.asm
+++ Version1/bootloader.asm
@@ -7,11 +7,39 @@
 ;   This file contains an simple "Hello World" bootloader written in the NASM assembler.
 ;   It is able to print the hello world characters to the screen.
 ;
-bits    16          ; Tell NASM this is 16 bit code.
-org     0x7C00      ; tell MASM to start outputting stuff at ofset 0x7c00
+;   mov     Move
+;   or
+;   jz      Jump Zero
+;   int     Interupt
+;   jmp     Jump
+;   cli     Clear Interupt
+;   hlt     Halt Execution
+;   db      Define Byte
+;   lodsb   Load Data Segment Register
+;
+
+bits    16          ; Tell NASM the output should be in 16 bit instructions ( Real Mode ).
+org     0x7C00      ; Tell MASM the output should start writing at register offset 0x7C00.
+
+; Initiate the program
 boot:
     mov si, hello   ; Move content from hello to the source index register, so it can be used to stream to the sceen.
-    mov ah, 0x0E    ; Set accumilator register to 0x0E (Ascii controll code: Shift Out, SO)
+    mov ah, 0x0E    ; Set accumilator register to 0x0E (Ascii controll code: Shift Out, SO).
+
+; For each character in "Hello World!"
 loop:
-    lodsb           ;
-    or al, al       ; Is 
+    lodsb           ; Load byte from the data segment register into accumilator low byte register.
+    or  al, al      ; Is the value in the accumilator low byte register equal to zero ?
+    jz  halt        ; Yes (zero flag is set after arithmetic operation), jump to halt (stop executing).
+    int 0x10        ; Call set video mode BIOS interrupt.
+    jmp .loop       ; Jump back to the start of the loop.
+
+; Initialize output string.
+hello:
+    db "Hello World!", 0    ; Define byes representing the string hello world.
+
+
+times 510 - ($-$$) db 0     ; Fill remaining 510 bytes of MBR with zeros to prevent unexpeced behaveour.
+
+
+dw 0xAA55 ; Mark this sector (the 512 bytes used in the bootleader) as bootable with the magic constand.
