2e8ea37a69c6b5752c1901adddc4d1a8600fc2b8
joris <jorisrietveld@gmail.com>
2017-11-25 05:36:51 +0100

    Added code to the bootloader2

--- Joris/NasmTutorial/OperatingSystem2/bootloader2.asm
+++ Joris/NasmTutorial/OperatingSystem2/bootloader2.asm
@@ -22,14 +22,23 @@ start:
 ;
 ;	OEM Parameter block (DDT Data Description Table)
 ;
-bpbOEM:                 db "Stendex "   ; This member must be exactally 8 bytes so it aligns correctly, and it contains the name of the oem
+bpbOEM:                 db "Stendex "   ; This member must be exactly 8 bytes so it aligns correctly, and it contains the name of the oem
 bpbBytesPerSector:  	DW 512          ; The number of bytes in each sector.
 bpbSectorsPerCluster: 	DB 1            ; The number of sectors in each cluster.
-bpbReservedSectors: 	DW 1            ;
-bpbNumberOfFATs: 	    DB 2            ;
-bpbRootEntries: 	    DW 224
-bpbTotalSectors: 	    DW 2880
-bpbMedia: 	            DB 0xF0
+bpbReservedSectors: 	DW 1            ; The number of reserved sectors (the boot sector is not part of the file system so there is 1)
+bpbNumberOfFATs: 	    DB 2            ; The number of FAT (File Allocation Tables) on the disk (FAT12 has a standard of 2 tables)
+bpbRootEntries: 	    DW 224          ; The maximum amount of directories inside the root directory (floppy disks have an standard limit of 224 files)
+bpbTotalSectors: 	    DW 2880         ; The total amount of sectors on the floppy disk.
+bpbMedia: 	            DB 0xF0         ; Remember hexdecimal: 0xF0  equals 1111 0000 in binary and
+                                        ; bit number: 7 6 5 4 3 2 1 0
+                                        ; bit value:  1 1 1 1 0 0 0 0
+                                        ; The media table contains settings about the properties of the disk, this are the options:
+                                        ; Bit 0: Sides/Heads    = 0 if it is single sided, 1 if its double sided
+                                        ; Bit 1: Size           = 0 if it has 9 sectors per FAT, 1 if it has 8.
+                                        ; Bit 2: Density        = 0 if it has 80 tracks, 1 if it is 40 tracks.
+                                        ; Bit 3: Type           = 0 if its a fixed disk (Such as hard drive), 1 if removable (Such as floppy drive)
+                                        ; Bits 4 to 7 are unused, and always 1.
+
 bpbSectorsPerFAT: 	    DW 9
 bpbSectorsPerTrack: 	DW 18
 bpbHeadsPerCylinder: 	DW 2
@@ -38,7 +47,9 @@ bpbTotalSectorsBig:     DD 0
 bsDriveNumber: 	        DB 0
 bsUnused: 	            DB 0
 bsExtBootSignature: 	DB 0x29
-bsSerialNumber:	        DD 0xa0a1a2a3
+bsSerialNumber:	        DD 0x42424242   ; The meaning of life, the universe and everything...
+                                        ;Would you have thought writing a bootloader would be so philosophical.
+
 bsVolumeLabel: 	        DB "MOS FLOPPY "; The name of the image
 bsFileSystem: 	        DB "FAT12   "   ; The file system of the image (use FAT 12 because of size limitations and simplicity)
 
@@ -104,6 +115,33 @@ loader:
 
 	cli			; Clear all Interrupts
 	hlt			; halt the system
+
+;
+; Reset the bootloader ( go to the fist byte of the floppy image)
+;
+.reset:
+    mov ab, 0
+    mov dl, 0
+    int 0x13
+    jc .reset       ; Jump if the carry flag is set ( there was an error reset again )
+    mov ax, 0x1000
+    mov es, ax      ;
+    xor bx, bx
+
+;
+; Read the next sector from the floppy drive.
+;
+.readDisk:
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 1   ; read next sector on the same track
+    mov cl, 2   ; Read sector (2 because the first sector is already in use.)
+    mov dh, 0   ; Read from the first head
+    mov dl, 0   ; Read from the first drive (drive 0 is the floppy drive)
+    int 0x13    ; Fire 0x13 BIOS interrupt, this interrupt is called read sector and uses the arguments above to read an sector.
+    jc .readDisk
+
+
 
 times 510 - ($-$$) db 0		; We have to be 512 bytes. Clear the rest of the bytes with 0
 
