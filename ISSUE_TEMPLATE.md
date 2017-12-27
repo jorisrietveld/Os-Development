2da7b16a111edb14530829561dca4458ccee5dd3
Joris Rietveld <jorisrietveld@gmail.com>
2017-12-27 13:09:52 +0100

    Creating println with video memory.

--- ISSUE_TEMPLATE.md
+++ ISSUE_TEMPLATE.md
@@ -1,37 +1,17 @@
-### Type: _Bug/Question/Feature request/Comment_
-> Description: 
-This is only an example it is not required and I added it to comply with the GitHub community standards.
-If you are reporting an bug please fill in the Expected behavour, Actual behavour, Platform Info and additional Info and 
-a good description like:
+### Type:
+Bug/Question/Feature request/Comment
 
-> Example: I edited the build.sh script and wanted to cheance the temporary floppy mount directory so I cheanced the `$FD` 
-variable from `$FD="/tmp/floppy-drive"`it to: 
-```
-$FD="/ tmp/disk-clean-up"
-rm -rf $FD || exit
-mkdir $FD && mount -o loop -t vfat jorix.flp $FD && cp stage2.bin kernel.bin $FD || exit
-umount /tmp/tmp-floppy-loop && rm -rf $FD || exit
-```
+### Description:
 
-### Exprected bahavour
-> Example:
- The creation of an directory in the tmp folder to mount my floppy image.
-
-### Actual behavour
-> Example:
- I got suddenly a lot of more free space on my hard disk drive, but I am having trubble finding my all my unbacked-up photos
- from tha past 10 years.
-
+You can remove everything below if you're not submitting a bug.
+### Expected behaviour
+### Actual behaviour
 ### Platform Info
-> Example:
-- *OS:* _Linux_
-- *OS Architecture* _x86_64_
-- *Nasm Version:* _2.13.02_
-- *Disc utils:* _mkdosfs, dd, fdutil, genisoimage_
-- *Commandline version:* _Bash 4.4.12(1)_
-- *Emulator(s)* _qemu-system-i386,qemu-system-x86_64, Bochs, Virtualbox_
-
+- *OS Family:* Linux/BSD/OSX/Windows
+- *OS distribution & version:* Debian/Arch/Slachware/Ubuntu/Fedora/OpenBSD/NetBSD/Darwin/Windows 10 pro/ etc.
+- *OS Architecture:* x86_64/i386
+- *Nasm Version:*
+- *Disc Image program:*
+- *Commandline version:*
+- *Emulator(s)*
 ### Additional Info
-> Example:
-_Is there something else that will maybe help me to find the bug?_
-_Example: While I encounted this bug that crashed the computter, I was drunk and spend a liter of beer over the CPU_
--- JorixOS/src/boot/libs/stdio.asm
+++ JorixOS/src/boot/libs/stdio.asm
@@ -24,9 +24,6 @@
 ;   string printing operations keyboard input etc.                                                                     ;
 ;   Created: 20-12-2017 07:25                                                                                          ;
 ;                                                                                                                      ;
-%ifndef STD_IO_16
-%define STD_IO_16
-
 bits 16
 
 ;_________________________________________________________________________________________________________________________/ ϝ put_string_16
@@ -46,4 +43,73 @@ put_string_16:
 		popa            ; restore registers
 		ret	            ; we are done, so return
 
-%endif
+;_________________________________________________________________________________________________________________________/ ϝ put_string_16
+;   This function prints a string to the screen.
+bits 32
+
+%define     VIDEO_MEMORY    0xB8000     ; The address maped to video memory in VGA mode 7.
+%define     COLUMNS         80          ; The amount of columns on the screen.
+%define     LINES           25          ; The amount of lines on the screen.
+%define     CHAR_ATTRIBUTE  14          ; The default character attribute (white char on black background)
+    ; Bits
+    ; 0-2   Foreground color of an character.
+    ; 0     red
+    ; 1     green
+    ; 2     blue
+    ; 3     The intensity of the foreground color.
+    ; 4-6   Background color of an character.
+    ; 4     red
+    ; 5     green
+    ; 6     blue
+    ; 7     Background intensity or let the character blink depending on the VGA implementation.
+_cursorPositionX db 0
+_cursorPositionY db 0
+
+;_________________________________________________________________________________________________________________________/ ϝ putChar32
+;   This function prints a string to the screen.
+;   Input registers: bl for an character.
+putChar32:
+    pusha                           ; Save the current CPU register state.
+    mov edi, VIDEO_MEMORY           ; Initiate the data input at the start of the video memory.
+
+    ; Get the current cursor position
+    xor eax, eax                    ; Reset the
+    mov ecx, COLUMNS * 0x02         ; Set the column width to char width(2byte) * columns
+    mov al, byte[_cursorPositionY]  ; Get the current y position of the cursor.
+    mul ecx                         ; Multiply the y position by the
+    push eax                        ; Save the calculated value on the stack.
+
+    mov al, byte[_cursorPositionX]  ; Get the x position of the cursor.
+    mov cl, 0x02                    ; Get the width of an char (2 bytes)
+    mul cl                          ; Multiply the cursor x position by the width.
+    pop eax                         ; Push the calculated value on to the stack.
+    add eax, ecx                    ; Add the character position to calculate the offset.
+
+    xor ecx, ecx                    ; Clear the counter register.
+    add edi, eax                    ; Add the offset to the base.
+
+    ; Check for new line cha_cursorPositionY characters.
+    cmp bl, 0x0A                    ; Do we need to print an newline character?
+    je .row                         ; Yes, then jump to the newline routine.
+
+    ; Print the character
+    mov dl, bl                      ; Set the function input to the data low register.
+    mov dh, CHAR_ATTRIBUTE          ; Set the character attribute to data high.
+    mov word[edi], dx               ; Write the character with attributes to the video memory.
+
+    ; Update the position
+    inc byte[_cursorPositionX]      ; Increment the position x tracker.
+    cmp [_cursorPositionY], COLUMNS ; Check if we are at the end of the line.
+    je .row                         ; If so insert an new line feed.
+    jmp .done                       ; Else jump to the exit routine.
+
+    ; Move to an new line feed.
+    .row:
+    mov byte[_cursorPositionX], 0x00; Go back to column 0 on the new line.
+    inc byte[_cursorPositionY]      ; Increment the line number.
+
+    ; Finished printing, restore the CPU registers and return to the caller.
+    .done:
+
+
+
